8255bd440203d3eb4421fa7cdb0e8159333e15ed
Özay Can Kırlı <ozaycank97@gmail.com>
2023-10-09 20:18:31 +0300

    City Component and Service added

--- src/app/models/city.ts
+++ src/app/models/city.ts
@@ -1,10 +1,10 @@
 import { Photo } from "./photo";
 
 export class City {
-    id:number;
-    name:string;
-    description:string;
-    userId:number;
-    photos:Photo[];
-
+    id!: number;
+    name!: string;
+    description!: string;
+    userId!: number;
+    photoUrl!: string;
+    photos!: Photo[];
 }
---
+++ src/app/services/city.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { City } from '../models/city';
+
+@Injectable()
+export class CityService {
+
+    constructor(private httpClient: HttpClient) { }
+    path = "https://localhost:7098/api/";
+    getCities(): Observable<City[]> {
+        return this.httpClient.get<City[]>(this.path + "cities");
+    }
+
+}
